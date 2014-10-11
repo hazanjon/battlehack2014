0fafd795cc9af8233fab095e0f8150761e9471cf
Jon Hazan <jon@hazan.me>
2014-10-12 01:16:36 +0400

    Voice with two users now working

--- app.js
+++ app.js
@@ -10,12 +10,8 @@ app.use('/', router);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var currentCalls =[
-	{
-		id: 3,
-		connected: [1]
-	}
-];
+var connections = [];
+var currentRooms =[];
 
 var users = [
 	{
@@ -23,7 +19,17 @@ var users = [
 		name: 'Test',
 		phone: settings.test_number,
 		lang: settings.test_lang,
-		currentCall: 3
+		currentRoom: 3,
+		message_queue: []
+	},
+	{
+		id: 2,
+		name: 'Jake',
+		phone: '+447584067712',
+		lang: 'de-DE',
+		currentRoom: 3,
+		message_queue: [
+		]
 	}
 ];
 
@@ -32,8 +38,35 @@ var helpers = {};
 helpers.findUserByPhone = function(phone) {
 	for (var i = 0, len = users.length; i < len; i++) {
 		var user = users[i];
-			if (user.email === email) {
+			if (user.phone === phone) {
 				return user;
+		}
+	}
+	return null;
+}
+helpers.findUserById = function(id) {
+	for (var i = 0, len = users.length; i < len; i++) {
+		var user = users[i];
+			if (user.id === id) {
+				return user;
+		}
+	}
+	return null;
+}
+helpers.findRoomById = function(room) {
+	for (var i = 0, len = currentRooms.length; i < len; i++) {
+		var currentRoom = currentRooms[i];
+			if (currentRoom.id === room) {
+				return currentRoom;
+		}
+	}
+	return null;
+}
+helpers.langById = function(langs, lang_id) {
+	for (var i = 0, len = langs.length; i < len; i++) {
+		var lang = langs[i];
+			if (lang.lang_id === lang_id) {
+				return lang;
 		}
 	}
 	return null;
@@ -54,6 +87,9 @@ twilio.receiveCall = function(req, res) {
 		case "record":
 			twilio.callRecording(req, res);
 		break;
+		case "startrecord":
+			twilio.callStartRecording(req, res);
+		break;
 		default:
 			twilio.callInitial(req, res);
 		break;
@@ -64,7 +100,8 @@ twilio.callInitial = function(req, res){
 
 	console.log("Voice Start");
     res.type('text/xml');
-    res.send('<Response><Say>Hello there! Talk when you hear the tone, press any key to finish recording</Say><Redirect method="GET">/api/twilio/voice?action=loop</Redirect></Response>');
+    //<Say>Hello there! Talk when you hear the tone, press any key to finish recording</Say>
+    res.send('<Response><Say>Hi</Say><Redirect method="GET">/api/twilio/voice?action=loop</Redirect></Response>');
 }
 
 twilio.callLoop = function(req, res){
@@ -72,38 +109,50 @@ twilio.callLoop = function(req, res){
 	console.log("Voice Loop");
 	res.type('text/xml');
 
+	var response = '<Response>';
+
 	//@TODO: Check if outstanding message to play
+	var number = (req.query.From == settings.twilio.number) ? req.query.To : req.query.From;
+	console.log(req.query.From, number);
+
+	var currentuser = helpers.findUserByPhone(number);
+	console.log("User Queue", currentuser.message_queue);
+
+	if(currentuser.message_queue.length){
+		for (var i = 0, len = currentuser.message_queue.length; i < len; i++) {
+			response += '<Play>'+currentuser.message_queue[i].file+'</Play>';
+		}
+	}
+
+	currentuser.message_queue = [];
+	console.log('Check for record');
 	//Record
-	res.send('<Response><Record action="/api/twilio/voice?action=record" timeout="1" method="GET"/><Redirect method="GET">/api/twilio/voice?action=loop</Redirect></Response>');
+	//response += '<Gather action="/api/twilio/voice?action=startrecord" method="GET" timeout="1" finishOnKey="0123456789*#"></Gather>';
+	response += '<Record action="/api/twilio/voice?action=record" timeout="1" method="GET"/>';
+
+	console.log(response);
+
+	res.send(response+'<Redirect method="GET">/api/twilio/voice?action=loop</Redirect></Response>');
+
+}
+twilio.callStartRecording = function(req, res){
+
+	console.log("Start Voice Record");
+	res.type('text/xml');
+	res.send('<Response><Say>Record now</Say><Record action="/api/twilio/voice?action=record" timeout="2" method="GET"/><Redirect method="GET">/api/twilio/voice?action=loop</Redirect></Response>');
 
 }
 twilio.callRecording = function(req, res){
 
-	console.log("Voice Record", req.query.RecordingUrl);
+	console.log("Voice Record", req.query);
 
-	var url = settings.translation_api.url + '/speech?api='+settings.translation_api.api_key+'&source=en-GB&target=de-DE&location=http://api.twilio.com/2010-04-01/Accounts/AC6ecab69649ef42b88af8e3a992e9f325/Recordings/RE7967b47aa851d42a6b7ddae0deccc54d&output=speech';
+	var number = (req.query.From == settings.twilio.number) ? req.query.To : req.query.From;
+	var currentuser = helpers.findUserByPhone(number);
+	console.log("User", currentuser);
 
-	console.log("Translation URL", url);
-	var request = http.get(url, function(response) {
-			console.log('status', response.statusCode);
-			
-			if(response.statusCode !== 200){
-				console.log('http error');
-			}else{
-				var body = '';
-				response.on('data', function(chunk) {
-			    	console.log(chunk);
-					body += chunk;
-				});
-				response.on('end', function() {
-			    	console.log(body);
-			    	body = JSON.parse(body);
-					res.type('text/xml');
-					res.send('<Response><Play>'+body.file+'</Play><Redirect method="GET">/api/twilio/voice?action=loop</Redirect></Response>');
-				});
-			}
-	});
-
+	distribute_message(currentuser, req.query.RecordingUrl, 'speech');
+	res.type('text/xml');
+	res.send('<Response><Say>Sent</Say><Redirect method="GET">/api/twilio/voice?action=loop</Redirect></Response>');
 
 }
 
@@ -139,29 +188,20 @@ twilio.receiveMessage = function(req, res) {
 	//expected: Text parts [ 'Call', '0123456789', 'german' ]
 
 	//TODO: Lookup initial user here
-	var user = {
-		phone: settings.test_number,
-		lang: settings.test_lang
-	}
+	var user = helpers.findUserByPhone(req.query.From);
+	var user2 = helpers.findUserByPhone(parts[1]);
+
 	//TODO: Send error if not auth
 
+	//var roomUsers = [user, user2];
+	var roomUsers = [user, user2];
 
-	var intent = {
-		action: "intiate_comms",
-		from: user.phone,
-		from_method: "twilio",
-		from_lang: user.lang,
-		to: parts[1],
-		to_method: "twilio",
-		to_lang: parts[2]
-	};
-
-	intentHandler.process(intent);
+	createRoom(roomUsers);
 
 	res.send("Message Success");
        
 }
-twilio.sendMessage = function(to, message) {
+twilio.sendSMS = function(to, message) {
 	twilio.client.sendMessage({
 
 	    to: to, // Any number Twilio can deliver to
@@ -170,6 +210,22 @@ twilio.sendMessage = function(to, message) {
 
 	});
 }
+
+twilio.connect = function(user){
+	twilio.client.makeCall({
+	    to: user.phone, // Any number Twilio can call
+	    from: settings.twilio.number, // A number you bought from Twilio and can use for outbound communication
+	    url: 'http://bh2014.hazan.me/api/twilio/voice' // A URL that produces an XML document (TwiML) which contains instructions for the call
+
+	}, function(err, responseData) {
+
+	    //executed when the call has been initiated.
+	    console.log(responseData.from); // outputs "+14506667788"
+
+	});
+}
+
+connections.twilio = twilio;
 
 var intentHandler = {};
 
@@ -187,37 +243,113 @@ intentHandler.process = function(intent){
 	}
 }
 
-var distribute_message = function(){
+var createRoom = function(users){
+	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+	    return v.toString(16);
+	});
+	var newroom = {
+		id: uuid,
+		connected: []
+	}
 
+	currentRooms.push(newroom);
+
+	for (var i = 0, len = users.length; i < len; i++) {
+		var usr = {
+			user_id: users[i].id,
+			method: 'twilio'
+		}
+		newroom.connected.push(usr);
+
+		users[i].currentRoom = uuid;
+
+		connections[usr.method].connect(users[i]);
+	}
+
+
+	return uuid;
 }
 
-var test = function(req, res) {
-	var url = "http://178.62.47.16/speech?api=1234&source=en-GB&target=de-DE&location=http://api.twilio.com/2010-04-01/Accounts/AC6ecab69649ef42b88af8e3a992e9f325/Recordings/RE7967b47aa851d42a6b7ddae0deccc54d&output=speech";
-	var request = http.get(url, function(response) {
-			console.log('status', response.statusCode);
-			
-			if(response.statusCode !== 200){
-				console.log('http error');
-			}else{
-				var body = '';
-				response.on('data', function(chunk) {
-			    	console.log(chunk);
-					body += chunk;
-				});
-				response.on('end', function() {
-			    	console.log(body);
-			    	body = JSON.parse(body);
-					res.type('text/xml');
-					res.send('<Response><Play>'+body.file+'</Play><Redirect method="GET">/api/twilio/voice?action=loop</Redirect></Response>');
-				});
+var distribute_message = function(currentuser, message, message_encoding){	
+
+	console.log('Distribution Message', message, message_encoding);
+	var currentroom = helpers.findRoomById(currentuser.currentRoom);
+	console.log('Room', currentroom);
+
+	var langs = [];
+
+	for (var i = 0, len = currentroom.connected.length; i < len; i++) {
+		var user = helpers.findUserById(currentroom.connected[i].user_id);
+
+		if(user.id == currentuser.id)
+			continue;
+
+		console.log('Room User', user);
+		var thislang = helpers.langById(langs, user.lang);
+		if(thislang == null){
+			var thislang = {
+				lang_id: user.lang,
+				users: []
 			}
-	});
+			langs.push(thislang);
+		}
+
+		thislang.users.push(currentroom.connected[i]);
+	}
+
+	console.log('thislang', thislang);
+
+	//thislang { lang_id: 'de-DE', users: [ [ [Object], [Object] ] ] }
+
+	for (var i = 0, len = langs.length; i < len; i++) {
+		targetlang = langs[i];
+		console.log(targetlang);
+		var url = settings.translation_api.url + '/'+message_encoding+'?api='+settings.translation_api.api_key+'&source='+currentuser.lang+'&target='+targetlang.lang_id+'&text='+message+'&output=speech';
+
+		console.log("Translation URL", url);
+		var request = http.get(url, function(response) {
+				console.log('status', response.statusCode);
+				
+				if(response.statusCode !== 200){
+					console.log('http error');
+				}else{
+					var body = '';
+					response.on('data', function(chunk) {
+						body += chunk;
+					});
+					response.on('end', function() {
+				    	console.log(body);
+				    	body = JSON.parse(body);
+
+				  //   	{
+						// status: "success",
+						// translation: "i wanna be the very best",
+						// file: "http://178.62.47.16/uploads/3dcf7f6ecc1ca33dd662cab6cae76170.mp3",
+						// timetaken: 1.0170071125031
+						// }
+
+						//@TODO: pick message strat based on comms channel
+						if(body.status == "success"){
+							for (var i = 0, len = targetlang.users.length; i < len; i++) {
+								console.log('Distribute Message:', targetlang.users[i].user_id, body)
+								var user = helpers.findUserById(targetlang.users[i].user_id);
+								user.message_queue.push(body);
+							}
+						}else{
+
+							console.log('Error', body.reason);
+						}
+					});
+				}
+		});
+	}
 }
 
 
 router.get('/api/twilio/voice', twilio.receiveCall);
+router.post('/api/twilio/voice', twilio.receiveCall);
 router.get('/api/twilio/message', twilio.receiveMessage);
-router.get('/test', test);
 
 app.use(express.static(__dirname + '/htdocs'));
 
